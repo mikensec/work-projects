d8bb6462887dc49622943ee9aa525494ed7ccda7
Michael Nieto <mikensec@users.noreply.github.com>
2023-01-30 20:29:53 -0600

    Update Install-Emulator.sh

--- Install-Emulator.sh
+++ Install-Emulator.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 # update package index
-sudo apt update
+sudo apt-get update
 
 # install JDK
-sudo apt install default-jdk -y
+sudo apt-get install default-jdk -y
 
 # download Android SDK
-wget https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip
+wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 
 # extract Android SDK
-sudo apt install unzip
-unzip commandlinetools-linux-9477386_latest.zip -d android-sdk
+unzip sdk-tools-linux-4333796.zip -d android-sdk
 
 # set ANDROID_HOME environment variable
 echo 'export ANDROID_HOME=~/android-sdk' >> ~/.bashrc
 
 # set PATH environment variable
-echo 'export PATH=$PATH:$ANDROID_HOME/android-sdk/android-sdk/cmdline-tools/bin/' >> ~/.bashrc
-
+echo 'export PATH=$PATH:$ANDROID_HOME/tools' >> ~/.bashrc
+echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> ~/.bashrc
 
 # source bashrc
 source ~/.bashrc
@@ -26,4 +25,5 @@ source ~/.bashrc
 # install required packages with the Android SDK Manager
 yes | sdkmanager "platform-tools" "platforms;android-28" "emulator"
 
-
+# clean up
+rm sdk-tools-linux-4333796.zip
